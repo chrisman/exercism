9eb3eb76c360422bb781eb008d64881fb9bfaa19
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-13 11:30:46 -0700

    :camel: word count with reduce

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,10 +1,12 @@
+function objBuilder(acc, curr, idx, arr){
+  acc[curr] = (acc.hasOwnProperty(curr)) ? acc[curr] + 1 : 1
+  return acc
+}
+
 module.exports = function(){
   return {
     count: function(s){
-      return s.split(' ').reduce(function(acc, curr, idx, arr){
-        acc[curr] = 1
-        return acc
-      },{})
+      return s.trim().split(/\s+/).reduce(objBuilder,{})
     }
   }
 }
