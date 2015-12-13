d2a82d984e0cb5e5ff521907a314b571fea7546f
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-13 10:50:56 -0700

    :horse: wrrrd count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,10 @@
 module.exports = function(){
   return {
-    count: function(){
-      return { word : 1}
+    count: function(s){
+      return s.split(' ').reduce(function(acc, curr, idx, arr){
+        acc[curr] = 1
+        return acc
+      },{})
     }
   }
 }
