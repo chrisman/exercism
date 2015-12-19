6e30108ae8744edc21e19b31926061786da61e30
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-18 20:26:58 -0700

    2nd verse

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -13,12 +13,23 @@ var animalsAndRemarks = [
   }
 ]
 
+function addBridge(n){
+  return `She swallowed the ${animalsAndRemarks[n]["animal"]} to catch the ${animalsAndRemarks[n - 1]["animal"]}.\n`
+}
+
 function getFirstLine(o){
   return `I know an old lady who swallowed a ${o["animal"]}.\n${o["remark"]}\n`
 }
 
-foodchain.prototype.verse = function(n){
-  return getFirstLine(animalsAndRemarks[n - 1])
+foodchain.prototype.verse = function(num) {
+  num = num - 1
+  console.log(num);
+  var song = getFirstLine(animalsAndRemarks[num])
+  if (num > 0) {
+    song += addBridge(num)
+    song += animalsAndRemarks[num - 1]["remark"]+'\n'
+  }
+  return song
 }
 
 module.exports = foodchain
