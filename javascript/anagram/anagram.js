03f444730c33dffb400a912df62f2e487a037057
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-22 14:16:41 -0700

    anagram.v2

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,13 +3,13 @@ function Anagram(str){
   return
 }
 
-Anagram.prototype.matches = function(arr){
+Anagram.prototype.matches = function(input){
   var key = this.key
   var sortedKey = key.toLowerCase().split('').sort().join('')
+  if (!(Array.isArray(input)))
+    input = Array.prototype.slice.call(arguments)
 
-  arr = (typeof arr === 'string') ? Array.prototype.slice.call(arguments) : arr
-
-  return arr.filter(function(e){
+  return input.filter(function(e){
     var sortedElement = e.toLowerCase().split('').sort().join('')
     return (e.toLowerCase() !== key.toLowerCase()) && (sortedElement === sortedKey)
   })
