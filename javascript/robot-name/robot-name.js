64669ba076a8c52e3be5577a0d6904f0a6d453df
Christopher Brown <christopher.p.brown@gmail.com>
2016-04-20 16:32:46 -0600

    :zap:

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var compose = (f, g) => x => f(g(x));
 var numToChar = n => String.fromCharCode(n);
 var randomInRange = (min, max) => 
@@ -5,19 +7,36 @@ var randomInRange = (min, max) =>
 var randCharNum = randomInRange(65, 90);
 var randChar = compose(numToChar, randCharNum)
 var randNumNum = randomInRange(0, 9);
-var add = (x, y) => x + y;
-var robotName = () => add(
-  randChar(), add(
-    randChar(), 
-    add(
-      randNumNum(),
-      add(
-        randNumNum(),
-        randNumNum()
-      )
-    )
-  )
-);
+var robotName = () => 
+  randChar() + randChar() + randNumNum() + randNumNum() + randNumNum();
+var has = (arr) => (elem) => arr.indexOf(elem) !== -1;
+var arrayAdd = (arr) => (elem) => arr.push(elem);
+var arrayRemove = (arr) => (elem) => 
+  arr.slice(0, arr.indexOf(elem))
+    .push(arr.slice(arr.indexOf(elem) + 1));
 
+var robotParty = [];
+var partyHas = has(robotParty);
+var partyAdd = arrayAdd(robotParty);
+var uniqRobotName = () => {
+  var name;
+  do name = robotName();
+  while (partyHas(name));
+  partyAdd(name);
+  return name;
+};
+var partyRemove = arrayRemove(robotParty);
+
+class Robot{
+  constructor(){
+    this.name = uniqRobotName();
+  }
+
+  reset(){
+    var temp = this.name;
+    this.name = uniqRobotName();
+    partyRemove(temp);
+  }
+}
 
 module.exports = Robot;
