fe129f4b47058d6b8a2666acf80d463e34e180d1
Chris Brown <chris.brown@skookum.com>
2016-05-09 07:55:09 -0600

    :zap:

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,26 +1,24 @@
+const dict = {
+  'C': 'G',
+  'G': 'C',
+  'A': 'U',
+  'T': 'A'
+}
+
+var add = (x, y) => x + y;
+var lookup = e => dict[e];
+var reverse_lookup = e =>
+  Object.keys(dict)
+    .filter( i => dict[i] === e)
+    .reduce(add);
+var convert = f => 
+  s => Array.from(s)
+    .map(f)
+    .reduce(add);
+
 module.exports = function() {
   return {
-    toRna: function(s){
-      return s.split('').map(function(i){
-
-        return (i === 'C')
-          ? 'G' : (i === 'G')
-          ? 'C' : (i === 'A')
-          ? 'U' : (i === 'T')
-          ? 'A' : 'C'
-
-      }).join('')
-    },
-
-    toDna: function(s){
-      return s.split('').map(function(i){
-
-        return (i === 'C')
-          ? 'G' : (i === 'G')
-          ? 'C' : (i === 'U')
-          ? 'A' : (i === 'A')
-          ? 'T' : 'C'
-
-      }).join('')    }
+    toRna: convert(lookup),
+    toDna: convert(reverse_lookup)
   }
 }
