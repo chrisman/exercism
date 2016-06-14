8cea914fad886bea5c69e3c3184cea8fec51423a
Chris Brown <chris.brown@skookum.com>
2016-06-14 15:52:32 -0600

    :sparkles: bob!

--- elm/02-bob/Bob.elm
+++ elm/02-bob/Bob.elm
@@ -1,1 +1,31 @@
 module Bob exposing (..)
+import String
+import List
+
+-- TYPES
+type alias Query = String
+type alias Answer = String
+
+isYelling : Query -> Bool
+isYelling q =
+  q == String.toUpper q && q /= String.toLower q
+
+isQuestion : Query -> Bool
+isQuestion q =
+  String.endsWith "?" q
+
+isSilence : Query -> Bool
+isSilence q =
+  String.isEmpty (String.trim q)
+  
+cases = [
+  (isYelling, "Whoa, chill out!"),
+  (isQuestion, "Sure."),
+  (isSilence, "Fine. Be that way!")
+  ] 
+
+hey : Query -> Answer
+hey query =
+  case (List.head (List.filter (\c -> (fst c) query) cases)) of
+    Just c -> snd c
+    Nothing -> "Whatever."
