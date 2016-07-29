4c95c694b25fd7b18dd447f6569ef6f9b1a08134
Chris Brown <chris.brown@skookum.com>
2016-07-29 09:17:52 -0600

    start triangle

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -1,1 +1,8 @@
 module Triangle exposing (..)
+
+triangleKind : number -> number -> number -> Result String String
+triangleKind a b c =
+  let sides = Ok "yep"
+  in case sides of
+    Ok val -> Ok "equilateral"
+    Err msg -> Err "not a triangle"
