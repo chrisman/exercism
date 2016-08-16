8dd0dc2a740375d7d267bd25aab029c78cd0a136
Chris Brown <chris.brown@skookum.com>
2016-08-16 10:11:54 -0600

    recursive sieve

--- javascript/sieve/sieve.js
+++ javascript/sieve/sieve.js
@@ -1,15 +1,21 @@
-function sieve(len) {
-  this.array = Array.from({length: len})
-    .map((_, i) => i + 1) // fill the array
+function Sieve(len) {
+  this.list = Array.from({length: len})
+    .map((_, i) => i + 1) // populat list
     .filter(x => x > 1);  // start at `2`
 
-  this.primes = [];
-  
-  while (this.array.length) {
-    let h = this.array[0];
-    this.primes.push(h);
-    this.array = this.array.filter(x => x % h !== 0);
-  }
+  this.primes = this.sieve(this.list);
 }
 
-module.exports = sieve;
+Sieve.prototype.sieve = function(list, acc = []) {
+  if (0 === list.length) 
+    return acc;
+
+  acc.push(list[0]);
+
+  return this.sieve(
+    list.filter(x => x % list[0] !== 0), 
+    acc
+  );
+}
+
+module.exports = Sieve;
