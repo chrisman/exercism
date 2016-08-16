00dbb73acaf575944b0e878693aa0bdb4de81b81
Chris Brown <chris.brown@skookum.com>
2016-08-16 10:24:15 -0600

    More terse

--- javascript/sieve/sieve.js
+++ javascript/sieve/sieve.js
@@ -3,19 +3,17 @@ function Sieve(len) {
     .map((_, i) => i + 1) // populat list
     .filter(x => x > 1);  // start at `2`
 
-  this.primes = this.sieve(this.list);
+  this.primes = _sieve(this.list);
 }
 
-Sieve.prototype.sieve = function(list, acc = []) {
-  if (0 === list.length) 
-    return acc;
-
-  acc.push(list[0]);
-
-  return this.sieve(
-    list.filter(x => x % list[0] !== 0), 
-    acc
-  );
-}
+var _sieve = (list, acc = []) =>
+  (0 === list.length) 
+    ? acc     // return accumulated primes
+    : _sieve( // recur
+        // filter out list head and multiples
+        list.filter(x => x % list[0] !== 0), 
+        // add list head to accumulator
+        acc.concat(list[0])
+      );
 
 module.exports = Sieve;
