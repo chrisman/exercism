d214c9db93ca5ec2ed5e0ecf0d733d4aa127c42b
Chris Brown <chris.brown@skookum.com>
2016-09-21 16:21:44 -0600

    ch-ch-changes

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,17 +1,7 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
-  var output = input || 'World'
-  return "Hello, "+ output +"!"
+  return `Hello, ${input || 'World'}!`;
 };
 
 module.exports = HelloWorld;
